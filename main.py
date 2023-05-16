0898a9af90f05f46360dbe9b4e45e845a4231f0d
Tosha <pateltk245.ce@gmail.com>
2023-05-17 01:24:27 +0530

    done

--- main.py
+++ main.py
@@ -39,9 +39,12 @@ def transform_text(text):
                   
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
-
+try:
+    tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+    model = pickle.load(open('model.pkl', 'rb'))
+except:
+    st.error('Model Loading error. Please check the model file path')
+    
 with open('image_url.txt', 'r') as f:
     image = f.read()
 
